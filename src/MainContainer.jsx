38152e687ecf07894070d7697d3c7737a3955d56
aneel <ang2k.17@gmail.com>
2024-07-12 08:03:04 -0700

    ABCD

---
+++ src/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './MainContainer.css';
+
+const MainContainer = () => {
+  return (
+    <div className="main-container">
+      <h1>ABCD</h1>
+      <p>ABCD</p>
+      <img src="https://via.placeholder.com/400" alt="Blank Image for Assignment " />
+      <p>ABCD</p>
+    </div>
+  );
+}
+
+export default MainContainer;
